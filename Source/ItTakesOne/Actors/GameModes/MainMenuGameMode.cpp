b9d4019853e92a0f91c02945b88d69bf8ae35806
JB Ladera <domobaby10@gmail.com>
2023-11-25 23:39:14 -0600

    fix menu, add game modes to maps, set world settings

---
+++ Source/ItTakesOne/Actors/GameModes/MainMenuGameMode.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainMenuGameMode.h"
+
+#include "Blueprint/UserWidget.h"
+#include "ItTakesOne/MainMenuPlayerController.h"
+#include "ItTakesOne/MainMenuUI.h"
+
+AMainMenuGameMode::AMainMenuGameMode()
+{
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(
+		TEXT("/Game/Blueprints/BP_MainMenuPlayerController"));
+	if (PlayerControllerBPClass.Class)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
+
+void AMainMenuGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (MenuWidgetClass)
+	{
+		AMainMenuPlayerController* Controller = Cast<AMainMenuPlayerController>(GetWorld()->GetFirstPlayerController());
+		if (Controller)
+		{
+			MainMenuWidget = CreateWidget<UMainMenuUI>(Controller, MenuWidgetClass);
+
+			if (MainMenuWidget)
+			{
+				MainMenuWidget->AddToPlayerScreen();
+			}
+		}
+	}
+}
