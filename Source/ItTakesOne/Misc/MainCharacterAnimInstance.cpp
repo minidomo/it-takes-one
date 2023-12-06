c5f251245a2be83063c68960e26a9f13b448c922
JB Ladera <domobaby10@gmail.com>
2023-12-05 20:08:47 -0600

    use custom anim instance class

---
+++ Source/ItTakesOne/Misc/MainCharacterAnimInstance.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacterAnimInstance.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ItTakesOne/ItTakesOneCharacter.h"
+
+UMainCharacterAnimInstance::UMainCharacterAnimInstance()
+{
+}
+
+void UMainCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Character = Cast<AItTakesOneCharacter>(GetOwningActor());
+	if (Character)
+	{
+		CharacterMovement = Character->GetCharacterMovement();
+	}
+}
+
+void UMainCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!IsValid(Character)) { return; }
+
+	Velocity = CharacterMovement->Velocity;
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0).Length();
+	bFalling = CharacterMovement->IsFalling();
+
+	bMoving = !CharacterMovement->GetCurrentAcceleration().IsZero() && GroundSpeed > 3;
+	bDash = Character->IsDash();
+
+	bIdle = !(bMoving || bDash);
+}
